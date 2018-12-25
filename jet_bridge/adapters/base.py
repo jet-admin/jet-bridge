093482a19f7c268f1d254d04622713b7918e15e5
Denis K <f1nal@cgaming.org>
2018-12-25 19:11:31 +0300

    Refactor

--- jet_bridge/adapters/base.py
+++ jet_bridge/adapters/base.py
@@ -1,23 +1,8 @@
-from jet_bridge.models import data_types as types
-
 
 class Adapter(object):
-    data_types = []
-    default_data_type = types.TEXT
 
     def __init__(self, engine, session):
         self.engine = engine
         self.session = session
-
-    def map_data_type(self, value):
-        for rule in self.data_types:
-            if rule['operator'] == 'equals' and value == rule['query']:
-                return rule['date_type']
-            elif rule['operator'] == 'startswith' and value[:len(rule['query'])] == rule['query']:
-                return rule['date_type']
-        return self.default_data_type
-
-    def get_tables(self):
-        raise NotImplementedError
 
 registered_adapters = {}
