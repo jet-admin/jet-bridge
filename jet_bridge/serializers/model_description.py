fce88450eaca528f204b81295f95a303c5071ea9
Denis K <f1nal@cgaming.org>
2019-05-31 09:57:29 +0300

    Add null to description

--- jet_bridge/serializers/model_description.py
+++ jet_bridge/serializers/model_description.py
@@ -7,6 +7,7 @@ class ModelDescriptionFieldSerializer(Serializer):
     db_column = fields.CharField()
     field = fields.CharField()
     filterable = fields.BooleanField()
+    null = fields.BooleanField()
     editable = fields.BooleanField()
     params = fields.JSONField()
 
@@ -16,6 +17,7 @@ class ModelDescriptionFieldSerializer(Serializer):
             'db_column',
             'field',
             'filterable',
+            'null',
             'editable',
             'params',
         )
