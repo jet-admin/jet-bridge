e207a9b1f497eca31659b6b24e11dc618c98e296
Denis K <f1nal@cgaming.org>
2019-06-03 15:36:19 +0300

    FIx media url

--- jet_bridge/views/api.py
+++ jet_bridge/views/api.py
@@ -1,4 +1,4 @@
-from jet_bridge import VERSION
+from jet_bridge import VERSION, settings
 from jet_bridge.responses.base import Response
 from jet_bridge.views.base.api import APIView
 
@@ -6,9 +6,11 @@ from jet_bridge.views.base.api import APIView
 class ApiHandler(APIView):
 
     def get(self):
+        media_url_base = settings.MEDIA_BASE_URL + '{}' if settings.MEDIA_BASE_URL \
+            else self.build_absolute_uri('/media/{}')
         response = Response({
             'version': VERSION,
             'type': 'jet_bridge',
-            'media_url_template': self.build_absolute_uri('/media/{}')
+            'media_url_template': media_url_base
         })
         self.write_response(response)
