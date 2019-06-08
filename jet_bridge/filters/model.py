4bce2c142a16013a13b6af00bbeba77d56c927c8
Denis K <f1nal@cgaming.org>
2019-06-09 02:20:19 +0300

    Add relation filter

--- jet_bridge/filters/model.py
+++ jet_bridge/filters/model.py
@@ -1,5 +1,6 @@
 from jet_bridge.filters.filter_class import FilterClass
 from jet_bridge.filters.model_m2m import get_model_m2m_filter
+from jet_bridge.filters.model_relation import get_model_relation_filter
 from jet_bridge.filters.model_search import get_model_search_filter
 from jet_bridge.filters.model_segment import get_model_segment_filter
 from jet_bridge.filters.order_by import OrderFilter
@@ -9,12 +10,14 @@ def get_model_filter_class(Model):
     search_filter = get_model_search_filter(Model)
     model_m2m_filter = get_model_m2m_filter(Model)
     model_segment_filter = get_model_segment_filter(Model)
+    model_relation_filter = get_model_relation_filter(Model)
 
     class ModelFilterClass(FilterClass):
         _order_by = OrderFilter()
         _search = search_filter()
         _m2m = model_m2m_filter()
         _segment = model_segment_filter()
+        _relation = model_relation_filter()
 
         class Meta:
             model = Model
