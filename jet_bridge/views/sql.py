6a8bfb4a6bca0fd7da1b53e529710ca3b28cdc53
Denis K <f1nal@cgaming.org>
2019-06-09 10:05:13 +0300

    Add multiple sql support

--- jet_bridge/views/sql.py
+++ jet_bridge/views/sql.py
@@ -1,7 +1,7 @@
 from jet_bridge.exceptions.sql import SqlError
 from jet_bridge.permissions import HasProjectPermissions
 from jet_bridge.responses.base import Response
-from jet_bridge.serializers.sql import SqlSerializer
+from jet_bridge.serializers.sql import SqlSerializer, SqlsSerializer
 from jet_bridge.views.base.api import APIView
 
 
@@ -9,11 +9,12 @@ class SqlHandler(APIView):
     permission_classes = (HasProjectPermissions,)
 
     def post(self):
-        serializer = SqlSerializer(data=self.data)
+        serializer = SqlsSerializer(data=self.data) if 'queries' in self.data \
+            else SqlSerializer(data=self.data)
         serializer.is_valid(raise_exception=True)
 
         try:
-            response = Response(serializer.execute())
+            response = Response(serializer.execute(serializer.validated_data))
             self.write_response(response)
         except SqlError as e:
             self.set_status(400)
