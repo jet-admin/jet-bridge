7d1779801478afa4797551acfd74099afb8e3c02
Denis K <f1nal@cgaming.org>
2019-10-12 14:15:16 +0300

    Fix partial update

--- packages/jet_bridge_base/jet_bridge_base/views/mixins/update.py
+++ packages/jet_bridge_base/jet_bridge_base/views/mixins/update.py
@@ -19,3 +19,7 @@ class UpdateAPIViewMixin(object):
 
     def perform_update(self, serializer):
         serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
