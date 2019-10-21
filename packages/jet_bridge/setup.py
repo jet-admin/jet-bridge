fb3e3aacdd0fb7d4aef70fb6e08125dd7f28d4c5
Denis K <f1nal@cgaming.org>
2019-10-21 20:50:06 +0300

    Update base dependency

--- packages/jet_bridge/setup.py
+++ packages/jet_bridge/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'tornado==5.1.1',
         'six',
-        'jet-bridge-base==0.4.9',
+        'jet-bridge-base==0.5.0',
     ]
 
     return install_requires
--- packages/jet_django/setup.py
+++ packages/jet_django/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'Django',
         'requests',
-        'jet-bridge-base==0.4.9'
+        'jet-bridge-base==0.5.0'
     ]
 
     try:
