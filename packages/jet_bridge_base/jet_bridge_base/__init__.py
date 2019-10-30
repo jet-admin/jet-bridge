b5bc64fd3afdb717bcafd14ca281b1146459d4ea
Denis K <f1nal@cgaming.org>
2019-10-30 09:40:10 +0300

    Update version

--- packages/jet_bridge_base/jet_bridge_base/__init__.py
+++ packages/jet_bridge_base/jet_bridge_base/__init__.py
@@ -1,3 +1,3 @@
 from jet_bridge_base import settings
 
-VERSION = '0.5.6'
+VERSION = '0.5.7'
--- packages/jet_django/setup.py
+++ packages/jet_django/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'Django',
         'requests',
-        'jet-bridge-base==0.5.6'
+        'jet-bridge-base==0.5.7'
     ]
 
     try:
