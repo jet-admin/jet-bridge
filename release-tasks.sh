bd60266d9259cd7380eb87cf54385cdec02032f4
Denis K <f1nal@cgaming.org>
2019-10-30 12:03:18 +0300

    Try fix

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pip install --upgrade pip
+#pip install --upgrade pip
 
 # for PostgreSQL
-pip install psycopg2-binary==2.8.3
+#pip install psycopg2-binary==2.8.3
 # for MySQL
 #apt update
 #apt install libmysqlclient-dev python-dev
