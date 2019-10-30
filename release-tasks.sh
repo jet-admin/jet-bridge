9dec4478230f0824ace94c9a460a128df65c43a8
Denis K <f1nal@cgaming.org>
2019-10-30 12:11:34 +0300

    Reverse

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,7 +3,7 @@
 pip install --upgrade pip
 
 # for PostgreSQL
-#pip install psycopg2-binary==2.8.3
+pip install psycopg2-binary==2.8.3
 # for MySQL
 #apt update
 #apt install libmysqlclient-dev python-dev
