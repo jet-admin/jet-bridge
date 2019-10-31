5488d847b10bd7dcdb959d169478592202274f42
Denis K <f1nal@cgaming.org>
2019-10-31 14:43:18 +0300

    Remove unused

--- packages/jet_django/jet_django/management/commands/jet_reset_token.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import BaseCommand
-
-from jet_bridge_base.commands.reset_token import reset_token_command
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        reset_token_command()
