fa16ffb82b321e793bc66d85e67a5f183775060a
Denis K <f1nal@cgaming.org>
2019-11-06 16:08:20 +0300

    Update versions

--- packages/jet_bridge_base/jet_bridge_base/__init__.py
+++ packages/jet_bridge_base/jet_bridge_base/__init__.py
@@ -1,3 +1,3 @@
 from jet_bridge_base import settings
 
-VERSION = '0.5.9'
+VERSION = '0.6.0'
--- packages/jet_django/jet_django/__init__.py
+++ packages/jet_django/jet_django/__init__.py
@@ -1,2 +1,2 @@
-VERSION = '0.8.0'
+VERSION = '0.8.1'
 default_app_config = 'jet_django.apps.JetDjangoConfig'
