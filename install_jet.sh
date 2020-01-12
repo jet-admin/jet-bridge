40058be2bd4ca86af3549aae939655aa922cd6df
Denis Kildishev <f1nal@cgaming.org>
2020-01-12 15:06:17 +0300

    Update install_jet.sh

--- install_jet.sh
+++ install_jet.sh
@@ -6,7 +6,7 @@ set -e
 #
 # This script is meant for quick & easy install via:
 #
-#   sh <(curl -s https://raw.githubusercontent.com/jet-admin/jet-bridge/master/install_jet.sh)
+#   sh <(curl -s https://app.jetadmin.io/install_jet.sh)
 
 
 PROJECT=$1
