b0a67c36e5055878b2dd934ad3e0676c5f6cef44
Denis Kildishev <f1nal@cgaming.org>
2020-01-19 14:31:10 +0300

    Allow custom django database

--- packages/jet_django/jet_django/settings.py
+++ packages/jet_django/jet_django/settings.py
@@ -9,10 +9,11 @@ JET_CORS_HEADERS = getattr(settings, 'JET_CORS_HEADERS', 'corsheaders' not in se
 JET_MEDIA_FILE_STORAGE = getattr(settings, 'JET_MEDIA_FILE_STORAGE', settings.DEFAULT_FILE_STORAGE)
 JET_PROJECT = getattr(settings, 'JET_PROJECT', None)
 JET_TOKEN = getattr(settings, 'JET_TOKEN', None)
+JET_DJANGO_DATABASE = getattr(settings, 'JET_DJANGO_DATABASE', 'default')
 JET_DATABASE_ONLY = getattr(settings, 'JET_DATABASE_ONLY', None)
 JET_DATABASE_EXCEPT = getattr(settings, 'JET_DATABASE_EXCEPT', None)
 
-database_settings = settings.DATABASES.get('default', {})
+database_settings = settings.DATABASES.get(JET_DJANGO_DATABASE, {})
 database_engine = None
 
 if connection.vendor == 'postgresql':
