ee1cc840af7367c5564e12c62f492d37d8cc113a
Denis Kildishev <f1nal@cgaming.org>
2020-02-10 17:53:18 -0800

    Update db deps

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,6 +3,6 @@
 # for PostgreSQL
 pip install psycopg2-binary==2.8.3
 # for MySQL
-#apt update
-#apt install libmysqlclient-dev python-dev
-#pip install mysqlclient
+pip install mysqlclient
+# for Microsoft SQL
+pip install pyodbc
