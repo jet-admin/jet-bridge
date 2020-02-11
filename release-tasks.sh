c6c87466e2c91970edd8e92adeb6f24c8363f0d4
Denis Kildishev <f1nal@cgaming.org>
2020-02-10 18:06:58 -0800

    Try apt odbc

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-apt install gcc g++
 # for PostgreSQL
 pip install psycopg2-binary==2.8.3
 # for MySQL
 pip install mysqlclient
 # for Microsoft SQL
-pip install pyodbc
+#pip install pyodbc
+apt install python-pyodbc
