fc784fa92d5b59be118aa62db3289e808aa39838
Denis Kildishev <f1nal@cgaming.org>
2020-02-10 18:10:27 -0800

    Try build

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+apt install python-pyodbc
+apt install unixodbc unixodbc-dev
 # for PostgreSQL
 pip install psycopg2-binary==2.8.3
 # for MySQL
 pip install mysqlclient
 # for Microsoft SQL
-#pip install pyodbc
-apt install python-pyodbc
+pip install pyodbc
