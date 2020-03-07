349c66892db07aadf5e59371aebc107dbbe5f0d4
Denis Kildishev <f1nal@cgaming.org>
2020-03-06 17:36:30 -0800

    Update version

--- packages/jet_bridge/setup.py
+++ packages/jet_bridge/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'tornado==5.1.1',
         'six',
-        'jet-bridge-base==0.7.2',
+        'jet-bridge-base==0.7.3',
     ]
 
     return install_requires
--- packages/jet_django/jet_django/__init__.py
+++ packages/jet_django/jet_django/__init__.py
@@ -1,2 +1,2 @@
-VERSION = '0.9.6'
+VERSION = '0.9.7'
 default_app_config = 'jet_django.apps.JetDjangoConfig'
