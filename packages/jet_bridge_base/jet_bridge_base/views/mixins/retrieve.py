a3cb66f9524d1b5a112d9f2e9e0e8d5ffe1dd4a9
Denis Kildishev <f1nal@cgaming.org>
2020-10-29 10:44:11 +0300

    Fix retrieve concurrency

--- packages/jet_bridge_base/jet_bridge_base/views/mixins/retrieve.py
+++ packages/jet_bridge_base/jet_bridge_base/views/mixins/retrieve.py
@@ -1,12 +1,9 @@
-from tornado import gen
-
 from jet_bridge_base.responses.json import JSONResponse
 
 
 class RetrieveAPIViewMixin(object):
 
-    @gen.coroutine
     def retrieve(self, *args, **kwargs):
-        instance = yield self.get_object()
+        instance = self.get_object()
         serializer = self.get_serializer(instance=instance)
         return JSONResponse(serializer.representation_data)
