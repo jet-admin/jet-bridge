3c972f636c40548ca48ddd12a853431b7fd9edc2
Denis Kildishev <f1nal@cgaming.org>
2021-02-26 11:02:34 +0300

    Add mssql types

--- packages/jet_bridge_base/jet_bridge_base/utils/db_types.py
+++ packages/jet_bridge_base/jet_bridge_base/utils/db_types.py
@@ -5,12 +5,14 @@ from sqlalchemy.sql import sqltypes
 
 map_data_types = [
     {'query': sqltypes.VARCHAR, 'date_type': data_types.TEXT},
+    {'query': sqltypes.Unicode, 'date_type': data_types.TEXT},
     {'query': sqltypes.TEXT, 'date_type': data_types.TEXT},
     {'query': sqltypes.BOOLEAN, 'date_type': data_types.BOOLEAN},
     {'query': sqltypes.INTEGER, 'date_type': data_types.INTEGER},
     {'query': sqltypes.SMALLINT, 'date_type': data_types.INTEGER},
     {'query': sqltypes.BIGINT, 'date_type': data_types.INTEGER},
     {'query': sqltypes.NUMERIC, 'date_type': data_types.FLOAT},
+    {'query': sqltypes.DATE, 'date_type': data_types.DATE_TIME},
     {'query': sqltypes.DATETIME, 'date_type': data_types.DATE_TIME},
     {'query': sqltypes.TIMESTAMP, 'date_type': data_types.DATE_TIME},
     {'query': sqltypes.JSON, 'date_type': data_types.JSON},
@@ -21,6 +23,14 @@ try:
     from geoalchemy2 import types
     map_data_types.append({'query': types.Geometry, 'date_type': data_types.GEOMETRY})
     map_data_types.append({'query': types.Geography, 'date_type': data_types.GEOGRAPHY})
+except ImportError:
+    pass
+
+try:
+    from sqlalchemy.dialects import mssql
+    map_data_types.append({'query': mssql.BIT, 'date_type': data_types.BOOLEAN})
+    map_data_types.append({'query': mssql.VARBINARY, 'date_type': data_types.TEXT})
+    map_data_types.append({'query': mssql.SMALLDATETIME, 'date_type': data_types.DATE_TIME})
 except ImportError:
     pass
 
