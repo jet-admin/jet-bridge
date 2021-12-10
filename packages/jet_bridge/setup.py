cc80f9957876f0217f5b3e208fe1dc3e27b8db3d
Denis Kildishev <f1nal@cgaming.org>
2021-12-10 12:52:21 +0300

    Update version

--- packages/jet_bridge/setup.py
+++ packages/jet_bridge/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'tornado==5.1.1',
         'six',
-        'jet-bridge-base==1.0.5',
+        'jet-bridge-base==1.0.6',
     ]
 
     return install_requires
--- packages/jet_bridge_base/jet_bridge_base/__init__.py
+++ packages/jet_bridge_base/jet_bridge_base/__init__.py
@@ -1,3 +1,3 @@
 from jet_bridge_base import settings
 
-VERSION = '1.0.5'
+VERSION = '1.0.6'
