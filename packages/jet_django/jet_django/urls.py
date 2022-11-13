087bc02e4436c934cab9d1bf40a0e21432d2a861
Denis Kildishev <f1nal@cgaming.org>
2022-11-13 11:11:09 +0700

    Fix django 4 compatibility

--- packages/jet_django/jet_django/urls.py
+++ packages/jet_django/jet_django/urls.py
@@ -1,5 +1,3 @@
-from django.conf.urls import url
-
 from jet_bridge_base.views.api import ApiView
 from jet_bridge_base.views.external_auth.complete import ExternalAuthCompleteView
 from jet_bridge_base.views.external_auth.login import ExternalAuthLoginView
@@ -25,24 +23,46 @@ app_name = 'jet_django'
 def init_urls():
     router = Router()
 
-    router.register('models/(?P<model>[^/]+)/', route_view(ModelViewSet))
-    router.register('tables/(?P<table>[^/]+)/columns/', route_view(TableColumnView))
-    router.register('tables/', route_view(TableView))
+    router.register('models/<model>/', 'models/(?P<model>[^/]+)/', route_view(ModelViewSet))
+    router.register('tables/<table>/columns/', 'tables/(?P<table>[^/]+)/columns/', route_view(TableColumnView))
+    router.register('tables/', 'tables/', route_view(TableView))
 
-    extra_urls = [
-        url(r'^$', route_view(ApiView).as_view(), name='root'),
-        url(r'^register/', route_view(RegisterView).as_view(), name='register'),
-        url(r'^graphql/', route_view(GraphQLView).as_view(), name='graphql'),
-        url(r'^model_descriptions/', route_view(ModelDescriptionView).as_view(), name='model-descriptions'),
-        url(r'^sql/', route_view(SqlView).as_view(), name='sql'),
-        url(r'^messages/', route_view(MessageView).as_view(), name='message'),
-        url(r'^file_upload/', route_view(FileUploadView).as_view(), name='file-upload'),
-        url(r'^image_resize/', route_view(ImageResizeView).as_view(), name='image-resize'),
-        url(r'^reload/', route_view(ReloadView).as_view(), name='reload'),
-        url(r'^proxy_request/', route_view(ProxyRequestView).as_view(), name='proxy-request'),
-        url(r'^api/external_auth/login/(?P<app>[^/]+)/', route_view(ExternalAuthLoginView).as_view(), name='external-auth-login'),
-        url(r'^api/external_auth/complete/(?P<app>[^/]+)/', route_view(ExternalAuthCompleteView).as_view(), name='external-auth-complete'),
-    ]
+    try:
+        from django.urls import path
+
+        extra_urls = [
+            path('', route_view(ApiView).as_view(), name='root'),
+            path('register/', route_view(RegisterView).as_view(), name='register'),
+            path('graphql/', route_view(GraphQLView).as_view(), name='graphql'),
+            path('model_descriptions/', route_view(ModelDescriptionView).as_view(), name='model-descriptions'),
+            path('sql/', route_view(SqlView).as_view(), name='sql'),
+            path('messages/', route_view(MessageView).as_view(), name='message'),
+            path('file_upload/', route_view(FileUploadView).as_view(), name='file-upload'),
+            path('image_resize/', route_view(ImageResizeView).as_view(), name='image-resize'),
+            path('reload/', route_view(ReloadView).as_view(), name='reload'),
+            path('proxy_request/', route_view(ProxyRequestView).as_view(), name='proxy-request'),
+            path('api/external_auth/login/<app>/', route_view(ExternalAuthLoginView).as_view(), name='external-auth-login'),
+            path('api/external_auth/complete/<app>/', route_view(ExternalAuthCompleteView).as_view(),name='external-auth-complete'),
+        ]
+    except ImportError:
+        from django.conf.urls import url
+
+        extra_urls = [
+            url(r'^$', route_view(ApiView).as_view(), name='root'),
+            url(r'^register/', route_view(RegisterView).as_view(), name='register'),
+            url(r'^graphql/', route_view(GraphQLView).as_view(), name='graphql'),
+            url(r'^model_descriptions/', route_view(ModelDescriptionView).as_view(), name='model-descriptions'),
+            url(r'^sql/', route_view(SqlView).as_view(), name='sql'),
+            url(r'^messages/', route_view(MessageView).as_view(), name='message'),
+            url(r'^file_upload/', route_view(FileUploadView).as_view(), name='file-upload'),
+            url(r'^image_resize/', route_view(ImageResizeView).as_view(), name='image-resize'),
+            url(r'^reload/', route_view(ReloadView).as_view(), name='reload'),
+            url(r'^proxy_request/', route_view(ProxyRequestView).as_view(), name='proxy-request'),
+            url(r'^api/external_auth/login/(?P<app>[^/]+)/', route_view(ExternalAuthLoginView).as_view(), name='external-auth-login'),
+            url(r'^api/external_auth/complete/(?P<app>[^/]+)/', route_view(ExternalAuthCompleteView).as_view(), name='external-auth-complete'),
+        ]
+
+
 
     api_urls = router.urls + extra_urls
 
