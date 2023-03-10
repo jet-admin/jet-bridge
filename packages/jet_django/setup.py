5105562eaf60663b3fdea62ab5fa12a4aeac147b
Denis Kildishev <f1nal@cgaming.org>
2023-03-10 11:30:53 +0700

    Update version

--- packages/jet_django/setup.py
+++ packages/jet_django/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'Django',
         'requests',
-        'jet-bridge-base==1.7.6',
+        'jet-bridge-base==1.7.7',
     ]
 
     try:
