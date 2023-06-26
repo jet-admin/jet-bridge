311711fdc035733fbfa8a1a87d0741dec970ac13
Denis Kildishev <f1nal@cgaming.org>
2023-06-26 15:23:48 +0300

    Revert "Revert "Merge branch 'stage'""
    This reverts commit 5a95f33733b0890ef9a2140b2e1d0a7bea8a657b.

--- packages/jet_bridge/setup.py
+++ packages/jet_bridge/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'tornado==5.1.1',
         'six',
-        'jet-bridge-base==1.7.19',
+        'jet-bridge-base==1.7.20',
         'paramiko==2.8.1',
         'sshtunnel',
     ]
