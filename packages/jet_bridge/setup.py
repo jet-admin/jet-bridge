11cd735d1f00bd5fbc5e5a8ba7317624160c644e
Denis Kildishev <f1nal@cgaming.org>
2024-05-07 16:43:49 -0300

    Update version

--- packages/jet_bridge/setup.py
+++ packages/jet_bridge/setup.py
@@ -15,7 +15,7 @@ def get_install_requires():
     install_requires = [
         'tornado==5.1.1',
         'six',
-        'jet-bridge-base==1.9.2',
+        'jet-bridge-base==1.9.3',
         'paramiko==2.8.1',
         'sshtunnel',
     ]
