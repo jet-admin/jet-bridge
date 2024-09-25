aaccef83ea54c9cc0eb3f20ceaa611ca0585d004
Denis Kildishev <f1nal@cgaming.org>
2024-09-25 12:32:46 +0300

    Add execute sessions commit

--- packages/jet_bridge_base/jet_bridge_base/utils/timezones.py
+++ packages/jet_bridge_base/jet_bridge_base/utils/timezones.py
@@ -1,6 +1,7 @@
 from datetime import tzinfo, timedelta
 
 from jet_bridge_base.utils.queryset import get_session_engine
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class FixedOffsetTimezone(tzinfo):
@@ -22,24 +23,36 @@ class FixedOffsetTimezone(tzinfo):
 
 
 def fetch_postgresql_default_timezone(session):
-    cursor = session.execute('SELECT NOW()')
-    row = cursor.fetchone()
+    try:
+        cursor = session.execute('SELECT NOW()')
+        row = cursor.fetchone()
+        session.commit()
 
-    return row[0].tzinfo
+        return row[0].tzinfo
+    except SQLAlchemyError:
+        session.rollback()
 
 
 def fetch_mysql_default_timezone(session):
-    cursor = session.execute('SELECT TIMEDIFF(NOW(), UTC_TIMESTAMP)')
-    row = cursor.fetchone()
+    try:
+        cursor = session.execute('SELECT TIMEDIFF(NOW(), UTC_TIMESTAMP)')
+        row = cursor.fetchone()
+        session.commit()
 
-    return FixedOffsetTimezone(row[0])
+        return FixedOffsetTimezone(row[0])
+    except SQLAlchemyError:
+        session.rollback()
 
 
 def fetch_mssql_default_timezone(session):
-    cursor = session.execute('SELECT sysdatetimeoffset() as now')
-    row = cursor.fetchone()
+    try:
+        cursor = session.execute('SELECT sysdatetimeoffset() as now')
+        row = cursor.fetchone()
+        session.commit()
 
-    return row[0].tzinfo
+        return row[0].tzinfo
+    except SQLAlchemyError:
+        session.rollback()
 
 
 def fetch_default_timezone(session):
