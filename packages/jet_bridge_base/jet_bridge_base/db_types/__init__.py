bf7e36a9c64e6a32116413b82be281c0dd7f89a5
Denis Kildishev <f1nal@cgaming.org>
2024-11-07 13:03:38 +0300

    Add timezone update once an hour

--- packages/jet_bridge_base/jet_bridge_base/db_types/__init__.py
+++ packages/jet_bridge_base/jet_bridge_base/db_types/__init__.py
@@ -5,5 +5,5 @@ from .metadata_file import dump_metadata_file, remove_metadata_file
 from .queryset import desc_uniform, empty_filter, get_queryset_order_by, get_queryset_limit, apply_default_ordering, \
     queryset_count_optimized, queryset_aggregate, queryset_group, get_sql_aggregate_func_by_name, \
     get_sql_group_func_lookup, queryset_search
-from .timezones import apply_session_timezone
+from .timezones import fetch_default_timezone, apply_session_timezone
 from .mongo import MongoDeclarativeMeta, MongoColumn, MongoDesc, MongoQueryset, MongoSession
