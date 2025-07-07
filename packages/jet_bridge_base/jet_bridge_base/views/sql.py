ddf14ead510f86d708f4bd639117dfc368561f6d
Denis Kildishev <f1nal@cgaming.org>
2025-07-07 14:55:28 +0300

    Fixes SQL error responses

--- packages/jet_bridge_base/jet_bridge_base/views/sql.py
+++ packages/jet_bridge_base/jet_bridge_base/views/sql.py
@@ -1,11 +1,8 @@
-from jet_bridge_base.exceptions.sql import SqlError
-from jet_bridge_base.exceptions.validation_error import ValidationError
 from jet_bridge_base.permissions import HasProjectPermissions
 from jet_bridge_base.responses.json import JSONResponse
 from jet_bridge_base.serializers.sql import SqlSerializer, SqlsSerializer
 from jet_bridge_base.utils.track_database import track_database_async
 from jet_bridge_base.views.base.api import APIView
-from jet_bridge_base.status import HTTP_400_BAD_REQUEST
 
 
 class SqlView(APIView):
@@ -20,13 +17,6 @@ class SqlView(APIView):
         else:
             serializer = SqlSerializer(data=request.data, context={'request': request})
 
-        try:
-            serializer.is_valid(raise_exception=True)
-        except ValidationError as e:
-            return JSONResponse({'error': str(e)}, status=HTTP_400_BAD_REQUEST)
-
-        try:
-            result = serializer.execute(serializer.validated_data)
-            return JSONResponse(result)
-        except SqlError as e:
-            return JSONResponse({'error': str(e.detail)}, status=HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        result = serializer.execute(serializer.validated_data)
+        return JSONResponse(result)
